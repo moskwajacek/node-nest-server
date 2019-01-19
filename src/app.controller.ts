d527c9ac7b1f13c3264c783f8390b02b00626801
moskwajacek <moskwajacek@gmail.com>
2019-01-20 00:48:55 +0100

    env, and methods

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,50 @@
-import { Controller, Get } from '@nestjs/common';
+import {Body, Controller, Get, NotFoundException, Param, Post, Query} from '@nestjs/common';
 import { AppService } from './app.service';
+
+
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  private contacts = [
+    {id: 0, name: 'Piotr'},
+    {id: 1, name: 'Paweł'},
+    {id: 2, name: 'Adam'},
+  ];
+
+  @Get('contacts')
+  getContacts(@Query('q') query) {
+    if (query) {
+      const queryReq = new RegExp(query, 'i');
+      return this.contacts.filter(row => row.name.search(queryReq) >= 0);
+    }
+    return this.contacts;
+  }
+
+  @Get('contacts/:id')
+  getContact(@Param('id') id: string) {
+    const contact = this.contacts.find(c => c.id === parseInt(id, 10));
+    if (contact) {
+      return contact;
+    } else {
+      throw new NotFoundException('Contact not found');
+    }
+  }
+
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('contacts')
+  postContacts(@Body() data) {
+    const contact = {
+      name: '',
+      ...data,
+      id: this.contacts.length,
+    };
+    this.contacts.push(contact);
+    return contact;
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import * as moduleAlias from 'module-alias';
 moduleAlias.addPath(__dirname);
+import * as dotenv from "dotenv";
+dotenv.config();
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const PORT = process.env.PORT || 3000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(PORT);
+  console.log(`App listen on port: ${PORT}`);
 }
 bootstrap();
